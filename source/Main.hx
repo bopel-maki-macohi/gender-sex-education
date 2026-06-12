7bed93ce1eb1fb0483b8cd2a2e81b2ad7fae178c
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-12 01:43:14 -0500

    skip splash + splash art wip, need flora char

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, gse.SplashState));
+		addChild(new FlxGame(0, 0, gse.SplashState, 60, 60, true, false));
 	}
 }
