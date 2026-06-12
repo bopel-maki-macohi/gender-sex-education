0a4bc192e5da5745dad91cfa6565eff0668d7e93
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-12 07:14:16 -0500

    get a list of files from a data folder

--- source/gse/lessons/LessonSelect.hx
+++ source/gse/lessons/LessonSelect.hx
@@ -1,5 +1,7 @@
 package gse.lessons;
 
+import haxe.io.Path;
+import lime.utils.Assets;
 import gse.lessonCategory.LessonCategorySelect;
 import flixel.FlxG;
 import gse.lessonCategory.LessonCategory;
@@ -14,6 +16,29 @@ class LessonSelect extends FlxState
 		super();
 
 		this.category = category;
+	}
+
+	var list:Array<String> = [];
+
+	override function create()
+	{
+		super.create();
+
+		for (file in Assets.list(TEXT))
+		{
+			// trace(Path.extension(file));
+			// trace(Path.directory(file));
+
+			if (Path.extension(file) != 'json')
+				continue;
+
+			if (Path.directory(file) != Paths.data('$category'))
+				continue;
+
+			list.push(file);
+		}
+
+		trace(list);
 	}
 
 	override function update(elapsed:Float)
